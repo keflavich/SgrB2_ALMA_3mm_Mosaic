edff8906ca5b73c5cb239686ce474471fa1a331c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-12-10 11:20:05 +0900

    frequency updates and rgb quicklooks

--- script/compare_hnc_hcn.py
+++ script/compare_hnc_hcn.py
@@ -1,29 +1,58 @@
-from spectral_cube import SpectralCube
+from spectral_cube import SpectralCube, LazyMask, BooleanArrayMask
 import copy
+from scipy import stats
 
 #cubehcn = SpectralCube.read('Feathered_HCN.fits').with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=88.6316024*u.GHz).spectral_slab(-95*u.km/u.s, 140*u.km/u.s)
 #cubehnc = SpectralCube.read('Feathered_HNC.fits').with_spectral_unit(u.km/u.s, velocity_convention='radio').spectral_slab(-95*u.km/u.s, 140*u.km/u.s)
 cubehcn = SpectralCube.read('SgrB2_a_03_7M.HCN.image.pbcor.fits').with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=88.6316024*u.GHz).spectral_slab(-90*u.km/u.s, 130*u.km/u.s)
+cubehcn = SpectralCube.read('SgrB2_a_03_7M.HCN.image.pbcor.fits').with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=88.63042*u.GHz).spectral_slab(-90*u.km/u.s, 130*u.km/u.s)
 cubehnc = SpectralCube.read('SgrB2_a_03_7M.HNC.image.pbcor.fits').with_spectral_unit(u.km/u.s, velocity_convention='radio').spectral_slab(-90*u.km/u.s, 130*u.km/u.s)
+#hcn_freqs = [88.63042, 88.6316, 88.63185, 88.63394, 88.6316024]*u.GHz
+#for f in hcn_freqs:
+#    vrange=[-16,120]*u.km/u.s
+#    cubehnc = SpectralCube.read('SgrB2_a_03_7M.HNC.image.pbcor.fits').with_spectral_unit(u.km/u.s, velocity_convention='radio').spectral_slab(vrange[0],vrange[1])
+#    cubehcn = SpectralCube.read('SgrB2_a_03_7M.HCN.image.pbcor.fits').with_spectral_unit(u.km/u.s, velocity_convention='radio', rest_value=f).spectral_slab(vrange[0],vrange[1])
+#    cubehcn=cubehcn.with_mask(BooleanArrayMask(cubehnc.mask.include(), cubehcn.wcs))
+#    print f, stats.pearsonr(cubehnc.flattened(), cubehcn.flattened())
+#    88.63042 GHz (0.52182746, 0.0)
+#    88.6316 GHz (0.49761587, 0.0)
+#    88.63185 GHz (0.48257208, 0.0)
+#    88.63394 GHz (0.3509686, 0.0)
+#    88.6316024 GHz (0.49761587, 0.0)
 
-cubehnc._wcs = cubehcn.wcs
-cubehnc.mask._wcs = cubehcn.wcs
+from FITS_tools import regrid_cube
+hnc_regrid = regrid_cube(cubehnc.filled_data[:], cubehnc.header, cubehcn.header)
+cubehnc = SpectralCube(data=hnc_regrid, wcs=cubehcn.wcs)
 
-mask  = (cubehcn > 0.5) & (cubehnc.mask)
-mask2 = (cubehcn > 0.5) & (cubehnc.mask)
-mask2._wcs = cubehnc.wcs
+#cubehnc._wcs = cubehcn.wcs
+#cubehnc.mask._wcs = cubehcn.wcs
+
+mask  = (cubehcn > 0.5)
+mask2 = (cubehcn > 0.5)
+#mask2._wcs = cubehnc.wcs
 hcn_flat = cubehcn.with_mask(mask).flattened()
 hnc_flat = cubehnc.with_mask(mask2).flattened()
 
 mask3 = cubehnc > 0.5
 mask4 = cubehnc > 0.5
-mask4._wcs = cubehcn.wcs
+#mask4._wcs = cubehcn.wcs
 hnc_flat2 = cubehnc.with_mask(mask3).flattened()
 hcn_flat2 = cubehcn.with_mask(mask4).flattened()
 
 import pylab as pl
 pl.clf()
-pl.plot(hnc_flat, hcn_flat, '.', alpha=0.05)
-pl.plot(hnc_flat2, hcn_flat2, '.', alpha=0.05)
+pl.plot(hnc_flat, hcn_flat, ',', alpha=0.5)
+pl.plot(hnc_flat2, hcn_flat2, ',', alpha=0.5)
 pl.plot([0,5],[0,5],'k--')
 pl.axis([0,5,0,5])
+
+dd = cubehcn.to_ds9()
+cubehnc.to_ds9(dd.id, newframe=True)
+dd.set('tile yes')
+dd.set('scale limits -0.5 2')
+dd.set('wcs fk5')
+dd.set('lock frame wcs')
+dd.set('lock slice image')
+dd.set('lock scale yes')
+dd.set('lock color yes')
+
--- script/vslice_quicklooks.py
+++ script/vslice_quicklooks.py
@@ -2,11 +2,22 @@ from astropy import units as u
 from spectral_cube import SpectralCube
 import pylab as pl
 import glob
+import os
 
 pl.ioff()
 
+def pdir(fn):
+    pathlist = os.path.split(fn)
+    pathlist = pathlist[:-1] + ("projections",) + pathlist[-1:]
+    return os.path.join(*pathlist)
+
+def pngdir(fn):
+    pathlist = os.path.split(fn)
+    pathlist = pathlist[:-1] + ("projections","pngs") + pathlist[-1:]
+    return os.path.join(*pathlist)
+
 for mol in ("HCN","HNC","HCOp","HC3N","H2CS303-202","H2CS303202"):
-    for vrange in ((30,51),(52,93),(94,134)):
+    for vrange in ((30,51),(52,93),(94,134),(55,66)):
         for fn in (glob.glob("*{0}*image.pbcor.fits".format(mol)) +
                    glob.glob("feathered/Feathered*{0}*.fits".format(mol)) +
                    glob.glob("feathered/Regridded*{0}*fits".format(mol))):
@@ -21,29 +32,44 @@ for mol in ("HCN","HNC","HCOp","HC3N","H2CS303-202","H2CS303202"):
             cube = rcube.with_mask(mask)
 
             cubemax = rcube.max(axis=0)
-            cubemax.quicklook(filename=fn.replace(".fits",".v{0}to{1}.max.png".format(*vrange)))
-            cubemax.hdu.writeto(fn.replace(".fits",".v{0}to{1}.max.fits".format(*vrange)),
+            cubemax.quicklook()
+            cubemax.FITSFigure.save(filename=pngdir(fn.replace(".fits",".v{0}to{1}.max.png".format(*vrange))))
+            cubemax.hdu.writeto(pdir(fn.replace(".fits",".v{0}to{1}.max.fits".format(*vrange))),
                                 clobber=True)
             cubemom0 = rcube.moment0(axis=0)
-            cubemom0.quicklook(filename=fn.replace(".fits",".v{0}to{1}.mom0.png".format(*vrange)))
-            cubemom0.hdu.writeto(fn.replace(".fits",".v{0}to{1}.mom0.fits".format(*vrange)),
+            cubemom0.quicklook()
+            cubemom0.FITSFigure.save(filename=pngdir(fn.replace(".fits",".v{0}to{1}.mom0.png".format(*vrange))))
+            cubemom0.hdu.writeto(pdir(fn.replace(".fits",".v{0}to{1}.mom0.fits".format(*vrange))),
                                  clobber=True)
 
 
             cubemom1 = cube.moment1(axis=0)
             cubemom1.quicklook()
             cubemom1.FITSFigure.show_colorscale(vmin=vrange[0], vmax=vrange[1])
-            cubemom1.FITSFigure.save(fn.replace(".fits",".v{0}to{1}.mom1.png".format(*vrange)))
-            cubemom1.hdu.writeto(fn.replace(".fits",".v{0}to{1}.mom1.fits".format(*vrange)),
+            cubemom1.FITSFigure.save(pngdir(fn.replace(".fits",".v{0}to{1}.mom1.png".format(*vrange))))
+            cubemom1.hdu.writeto(pdir(fn.replace(".fits",".v{0}to{1}.mom1.fits".format(*vrange))),
                                  clobber=True)
 
             cubemom2 = cube.moment2(axis=0)
             cubemom2.quicklook()
             cubemom2.FITSFigure.show_colorscale(vmin=00, vmax=50)
-            cubemom2.FITSFigure.save(fn.replace(".fits",".v{0}to{1}.mom2.png".format(*vrange)))
-            cubemom2.hdu.writeto(fn.replace(".fits",".v{0}to{1}.mom2.fits".format(*vrange)),
+            cubemom2.FITSFigure.save(pngdir(fn.replace(".fits",".v{0}to{1}.mom2.png".format(*vrange))))
+            cubemom2.hdu.writeto(pdir(fn.replace(".fits",".v{0}to{1}.mom2.fits".format(*vrange))),
                                  clobber=True)
 
             for ii in pl.get_fignums():
                 pl.close(ii)
 
+import aplpy
+for globstr in (("*{0}*image.pbcor.fits"),
+                ("feathered/Feathered*{0}*.fits"),):
+                #("feathered/Regridded*{0}*fits")):
+    for vrange,(zmin,zmax) in zip(((30,51),(52,93),(94,134),(55,66)),
+                             ((-3,35),(-5,60),(-5,40))):
+        rgbfn = [pdir(glob.glob(globstr.format(mol))[0].replace(".fits",".v{0}to{1}.mom0.fits".format(*vrange)))
+                 for mol in ("HCN","HNC","HCO[p+]")
+                 ]
+        out = pngdir(os.path.join(os.path.split(globstr)[0],"rgb_hcn_hnc_hcop.v{0}to{1}.mom0.png".format(*vrange)))
+        print rgbfn,out
+
+        aplpy.rgb.make_rgb_image(rgbfn, out, vmin_r=zmin, vmin_g=zmin, vmin_b=zmin, vmax_r=zmax, vmax_g=zmax, vmax_b=zmax)
