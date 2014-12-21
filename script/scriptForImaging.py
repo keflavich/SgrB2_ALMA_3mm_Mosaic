ca8db96bd5cb9b895ab6b5b0fc2e4f61c593b1ea
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-12-21 06:57:58 +0100

    update reduction scripts (HC3N debugging)

--- script/scriptForImaging.py
+++ script/scriptForImaging.py
@@ -162,22 +162,26 @@ exportfits(maskname,
            maskname+".fits", dropdeg=True,
            overwrite=True)
 
-clean(vis = 'SgrB2_a_03_7M.cal.contsub',
-  imagename = 'SgrB2_a_03_7M.HC3N.clean',
-  field = '0~52', # SgrB2
-  spw = '0,4,8,12',
-  mode = 'velocity', restfreq='90979.02MHz', start = start, width = '1.0km/s', nchan=nchan, outframe = 'lsrk',
-  imagermode = 'mosaic',
-  interactive = F,
-  imsize = [216, 216],
-  cell = '2.5arcsec',
-  phasecenter = 'J2000 17h47m19.4 -28d23m29',
-  mask = maskname,
-  weighting = 'briggs',
-  niter = 20000, threshold = '300mJy',
-  robust = 0.5, usescratch = True,
-  #minpb = 0.8, # attempt to mitigate 'inf' artifacts
-     )
+# Problems exist starting around 2000-3000
+for niter in (2000,20000):
+    os.system('rm -rf SgrB2_a_03_7M.HC3N.clean.niter{0}*'.format(niter))
+    clean(vis = 'SgrB2_a_03_7M.cal.contsub',
+      imagename = 'SgrB2_a_03_7M.HC3N.clean.niter{0}'.format(niter),
+      field = '0~52', # SgrB2
+      spw = '0,4,8,12',
+      mode = 'velocity', restfreq='90979.02MHz', start = start, width = '1.0km/s', nchan=nchan, outframe = 'lsrk',
+      imagermode = 'mosaic',
+      interactive = F,
+      imsize = [216, 216],
+      cell = '2.5arcsec',
+      phasecenter = 'J2000 17h47m19.4 -28d23m29',
+      #mask = maskname,
+      weighting = 'briggs',
+      niter = niter, threshold = '300mJy',
+      robust = 0.5, usescratch = True,
+      minpb = 0.4, # attempt to mitigate 'inf' artifacts
+      mask = True,
+         )
 
 
 
