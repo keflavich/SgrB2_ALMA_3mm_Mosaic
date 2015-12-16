d2e57c8513e57cbc95f5a89800e0b9e21864dc08
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-12-16 11:15:28 +0100

    better median subtraction, correct the velocity guesses

--- analysis/ch3cn_fiteach.py
+++ analysis/ch3cn_fiteach.py
@@ -1,7 +1,14 @@
 from ch3cn_fits import SpectralCube, pyspeckit, fits, u, np
+import os
 
 cubefn = '../FITS/merge/SgrB2_b3_7M_12M.CH3CN.image.pbcor_medsub.fits'
-cube = SpectralCube.read(cubefn).minimal_subcube()
+if not os.path.exists(cubefn):
+    cube = SpectralCube.read('../FITS/merge/SgrB2_b3_7M_12M.CH3CN.image.pbcor.fits').minimal_subcube()
+    med = cube.percentile(25, axis=0)
+    cube = cube - med
+    cube.write(cubefn)
+else:
+    cube = SpectralCube.read(cubefn).minimal_subcube()
 cubeK = cube.to(u.K, u.brightness_temperature(cube.beam,
                                               cube.wcs.wcs.restfrq*u.Hz))
 err = cubeK[:30].std(axis=0)
@@ -10,15 +17,15 @@ mask = (peak > 1*u.K) & (peak > 6*err)
 
 pcube = pyspeckit.Cube(cube=cubeK)
 
-vguesses = cube.spectral_axis[cube.spectral_slab(-50*u.km/u.s, 100*u.km/u.s)
-                              .argmax(axis=0)]
+subcube = cube.spectral_slab(-50*u.km/u.s, 100*u.km/u.s)
+vguesses = subcube.spectral_axis[subcube.argmax(axis=0)]
 colguesses = np.ones_like(mask)*1e15
 temguesses = np.ones_like(mask)*150.
 widths = np.ones_like(mask)*5.0
 guesses = np.array([vguesses.value, widths, temguesses, colguesses])
 
 # For laptop
-#mask &= (peak>10*u.K)
+mask &= (peak>10*u.K)
 
 start_point = np.unravel_index(np.nanargmax(peak*mask), peak.shape)
 
