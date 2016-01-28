8966d537713570c82150f35522c6019295756afe
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-01-28 09:21:37 +0100

    selfcal script for continuum

--- script_12m_te/selfcal_continuum.py
+++ script_12m_te/selfcal_continuum.py
@@ -1,24 +1,20 @@
+# REDO THIS FROM TESTS...
 import glob
 
 contvis='SgrB2_TE_contsplit.ms'
-centralpointingvis='SgrB2_TE_center.ms'
-phasecenter='J2000 17:47:20.129 -28.23.04.07'
-imsize=[1024,1024]
+selfcal0vis = 'selfcal_SgrB2_TE_full_selfcal_iter0.ms'
+phasecenter='J2000 17:47:19.242 -28.23.33.22'
+imsize=[4096,4096]
 
-
-ptg_numbers = [3, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 76, 77, 78, 79, 87,
-               88, 89, 90, 91, 98, 99, 100, 101, 102, 110, 111, 112, 113]
-
-rmtables(centralpointingvis)
-split(vis=contvis, outputvis=centralpointingvis,
-      field=",".join([str(x-3) for x in ptg_numbers]),
+rmtables(selfcal0vis)
+split(vis=contvis, outputvis=selfcal0vis,
       datacolumn='data')
 
-outname = 'SgrB2_center_TE'
+outname = 'SgrB2_selfcal_full_TE'
 
 rmtables(glob.glob(outname+".*"))
 myimagebase = outname
-tclean(vis=centralpointingvis,
+tclean(vis=selfcal0vis,
        imagename=myimagebase,
        field='SgrB2',
        gridder='mosaic',
@@ -42,15 +38,15 @@ exportfits(imagename=myimagebase+'.model', fitsimage=myimagebase+'.model.fits', 
 exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', dropdeg=True, overwrite=True) # export the PB image
 
 rmtables(['phase_0.cal'])
-gaincal(vis=centralpointingvis, caltable='phase_0.cal', solint='int', gaintype='G', calmode='p')
-selfcal1vis = 'selfcal_SgrB2_TE_centralpointing_iter1.ms'
+gaincal(vis=selfcal0vis, caltable='phase_0.cal', solint='int', gaintype='G', calmode='p')
+selfcal1vis = 'selfcal_SgrB2_TE_full_selfcal_iter1.ms'
 rmtables([selfcal1vis])
-applycal(vis=centralpointingvis, field="", gaintable=["phase_0.cal"],
+applycal(vis=selfcal0vis, field="", gaintable=["phase_0.cal"],
          interp="linear", applymode='calonly', calwt=False)
-split(vis=centralpointingvis, outputvis=selfcal1vis, datacolumn='corrected')
+split(vis=selfcal0vis, outputvis=selfcal1vis, datacolumn='corrected')
 
 
-outname = 'SgrB2_center_TE_selfcal1'
+outname = 'SgrB2_selfcal_full_TE_selfcal1'
 os.system('rm -rf ' + outname + "*")
 myimagebase = outname
 tclean(vis=selfcal1vis,
@@ -79,14 +75,14 @@ exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.f
 
 rmtables(['phase_1.cal'])
 gaincal(vis=selfcal1vis, caltable='phase_1.cal', solint='int', gaintype='G', calmode='p')
-selfcal2vis = 'selfcal_SgrB2_TE_centralpointing_iter2.ms'
+selfcal2vis = 'selfcal_SgrB2_TE_full_selfcal_iter2.ms'
 rmtables([selfcal2vis])
 applycal(vis=selfcal1vis, field="", gaintable=["phase_1.cal"],
          interp="linear", applymode='calonly', calwt=False)
 split(vis=selfcal1vis, outputvis=selfcal2vis, datacolumn='corrected')
 
 
-outname = 'SgrB2_center_TE_selfcal2'
+outname = 'SgrB2_selfcal_full_TE_selfcal2'
 os.system('rm -rf ' + outname + "*")
 myimagebase = outname
 tclean(vis=selfcal2vis,
@@ -116,14 +112,14 @@ exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.f
 
 rmtables(['phase_2.cal'])
 gaincal(vis=selfcal2vis, caltable='phase_2.cal', solint='int', gaintype='G', calmode='p')
-selfcal3vis = 'selfcal_SgrB2_TE_centralpointing_iter3.ms'
+selfcal3vis = 'selfcal_SgrB2_TE_full_selfcal_iter3.ms'
 rmtables([selfcal3vis])
 applycal(vis=selfcal2vis, field="", gaintable=["phase_2.cal"],
          interp="linear", applymode='calonly', calwt=False)
 split(vis=selfcal2vis, outputvis=selfcal3vis, datacolumn='corrected')
 
 
-outname = 'SgrB2_center_TE_selfcal3'
+outname = 'SgrB2_selfcal_full_TE_selfcal3'
 os.system('rm -rf ' + outname + "*")
 myimagebase = outname
 tclean(vis=selfcal3vis,
@@ -150,16 +146,21 @@ exportfits(imagename=myimagebase+'.model', fitsimage=myimagebase+'.model.fits', 
 exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', dropdeg=True, overwrite=True) # export the PB image
 
 
-# sanity check
-# should be identical to selfcal3
-applycal(vis=centralpointingvis, field="",
-         gaintable=["phase_0.cal","phase_1.cal","phase_2.cal"],
-         interp="linear", applymode='calonly', calwt=False)
 
-outname = 'SgrB2_center_TE_selfcal_applied_sanitycheck'
-rmtables(glob.glob(outname+".*"))
+rmtables(['ampphase_3.cal'])
+gaincal(vis=selfcal3vis, caltable='ampphase_3.cal', solint='int', gaintype='G',
+        calmode='ap')
+selfcal4vis = 'selfcal_SgrB2_TE_full_selfcal_iter4_ampphase.ms'
+rmtables([selfcal4vis])
+applycal(vis=selfcal3vis, field="", gaintable=["ampphase_3.cal"],
+         interp="linear", applymode='calonly', calwt=False)
+split(vis=selfcal3vis, outputvis=selfcal4vis, datacolumn='corrected')
+
+
+outname = 'SgrB2_selfcal_full_TE_selfcal4_ampphase'
+os.system('rm -rf ' + outname + "*")
 myimagebase = outname
-tclean(vis=centralpointingvis,
+tclean(vis=selfcal4vis,
        imagename=myimagebase,
        field='SgrB2',
        gridder='mosaic',
@@ -174,7 +175,7 @@ tclean(vis=centralpointingvis,
        cell="0.125arcsec",
        outframe='LSRK',
        weighting="briggs",
-       robust = 0.5, 
+       robust = 0.5,
        savemodel='modelcolumn')
 impbcor(imagename=myimagebase+'.image', pbimage=myimagebase+'.pb', outfile=myimagebase+'.image.pbcor', overwrite=True) # perform PBcorr
 exportfits(imagename=myimagebase+'.image.pbcor', fitsimage=myimagebase+'.image.pbcor.fits', dropdeg=True, overwrite=True) # export the corrected image
@@ -183,17 +184,46 @@ exportfits(imagename=myimagebase+'.model', fitsimage=myimagebase+'.model.fits', 
 exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', dropdeg=True, overwrite=True) # export the PB image
 
 
-rmtables(['ampphase_3.cal'])
-gaincal(vis=selfcal3vis, caltable='ampphase_3.cal', solint='int', gaintype='G',
+rmtables(['ampphase_4.cal'])
+gaincal(vis=selfcal3vis, caltable='ampphase_4.cal', solint='int', gaintype='G',
         calmode='ap')
-selfcal4vis = 'selfcal_SgrB2_TE_centralpointing_iter4_ampphase.ms'
-rmtables([selfcal4vis])
-applycal(vis=selfcal3vis, field="", gaintable=["ampphase_3.cal"],
+selfcal5vis = 'selfcal_SgrB2_TE_full_selfcal_iter5_ampphase.ms'
+rmtables([selfcal5vis])
+applycal(vis=selfcal4vis, field="", gaintable=["ampphase_4.cal"],
          interp="linear", applymode='calonly', calwt=False)
-split(vis=selfcal3vis, outputvis=selfcal4vis, datacolumn='corrected')
+split(vis=selfcal4vis, outputvis=selfcal5vis, datacolumn='corrected')
+# could try selfcal5vis....
 
 
-outname = 'SgrB2_center_TE_selfcal4'
+outname = 'SgrB2_selfcal_full_TE_selfcal5_ampphase'
+os.system('rm -rf ' + outname + "*")
+myimagebase = outname
+tclean(vis=selfcal5vis,
+       imagename=myimagebase,
+       field='SgrB2',
+       gridder='mosaic',
+       spw="",
+       phasecenter=phasecenter,
+       specmode="mfs",
+       niter=100000,
+       threshold="0.5mJy",
+       deconvolver="clark",
+       interactive=False,
+       imsize=imsize,
+       cell="0.125arcsec",
+       outframe='LSRK',
+       weighting="briggs",
+       robust = 0.5,
+       savemodel='modelcolumn')
+impbcor(imagename=myimagebase+'.image', pbimage=myimagebase+'.pb', outfile=myimagebase+'.image.pbcor', overwrite=True) # perform PBcorr
+exportfits(imagename=myimagebase+'.image.pbcor', fitsimage=myimagebase+'.image.pbcor.fits', dropdeg=True, overwrite=True) # export the corrected image
+exportfits(imagename=myimagebase+'.pb', fitsimage=myimagebase+'.pb.fits', dropdeg=True, overwrite=True) # export the PB image
+exportfits(imagename=myimagebase+'.model', fitsimage=myimagebase+'.model.fits', dropdeg=True, overwrite=True) # export the PB image
+exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', dropdeg=True, overwrite=True) # export the PB image
+
+
+# test to see if multiscale is any good
+outname = 'SgrB2_selfcal_full_TE_selfcal4_ampphase_multiscale'
 os.system('rm -rf ' + outname + "*")
 myimagebase = outname
 tclean(vis=selfcal4vis,
@@ -205,7 +235,8 @@ tclean(vis=selfcal4vis,
        specmode="mfs",
        niter=100000,
        threshold="0.5mJy",
-       deconvolver="clark",
+       deconvolver="multiscale",
+       scales=[0,4,12],
        interactive=False,
        imsize=imsize,
        cell="0.125arcsec",
