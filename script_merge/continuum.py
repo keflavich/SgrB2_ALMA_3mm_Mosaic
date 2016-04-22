e77539192d0c9cacfa5208a5bfc9f21e0737f8b7
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2016-04-22 11:54:48 -0300

    add tc+te merge script

---
+++ script_merge/continuum.py
@@ -0,0 +1,35 @@
+import os
+
+vis_tc='SgrB2_TC_contsplit.ms'
+#vis_te = 'TE/selfcal_SgrB2_TE_full_selfcal_iter4_ampphase.ms'
+vis_te='SgrB2_TE_contsplit.ms'
+
+phasecenter='J2000 17:47:19.242 -28.23.33.22'
+imsize=[4096,4096]
+
+outname = 'SgrB2_nocalTE_nocalTC_merge_continuum'
+os.system('rm -rf ' + outname + "*")
+myimagebase = outname
+
+tclean(vis=[vis_tc,vis_te],
+       imagename=myimagebase,
+       field='SgrB2',
+       gridder='mosaic',
+       spw="",
+       phasecenter=phasecenter,
+       specmode="mfs",
+       niter=100000,
+       threshold="0.5mJy",
+       deconvolver="clark",
+       interactive=False,
+       imsize=imsize,
+       cell="0.125arcsec",
+       outframe='LSRK',
+       weighting="briggs",
+       robust = 0.5,
+       savemodel='modelcolumn')
+impbcor(imagename=myimagebase+'.image', pbimage=myimagebase+'.pb', outfile=myimagebase+'.image.pbcor', overwrite=True) # perform PBcorr
+exportfits(imagename=myimagebase+'.image.pbcor', fitsimage=myimagebase+'.image.pbcor.fits', dropdeg=True, overwrite=True) # export the corrected image
+exportfits(imagename=myimagebase+'.pb', fitsimage=myimagebase+'.pb.fits', dropdeg=True, overwrite=True) # export the PB image
+exportfits(imagename=myimagebase+'.model', fitsimage=myimagebase+'.model.fits', dropdeg=True, overwrite=True) # export the PB image
+exportfits(imagename=myimagebase+'.residual', fitsimage=myimagebase+'.residual.fits', dropdeg=True, overwrite=True) # export the PB image
