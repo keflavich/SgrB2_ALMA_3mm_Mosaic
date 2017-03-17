b227bddb7a8c45cf218c92d372e78f2531713ba1
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-03-17 09:06:50 -0400

    +

--- analysis/extract_12m_mergecube.py
+++ analysis/extract_12m_mergecube.py
@@ -62,7 +62,7 @@ else:
 
 
 regions = (
-           pyregion.open(rpath('cores_with_names.reg')) +
+           pyregion.open(rpath('cores_with_names.reg'))
            #pyregion.open(rpath('tc_continuum_core_extraction_regions.reg')) +
            #pyregion.open(rpath('ionizationfront_circle.reg')) +
            #pyregion.open(rpath('extraction_regions_n_and_m.reg')) +
