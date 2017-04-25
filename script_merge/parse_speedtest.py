539023c9df7de93ab62ff1a7440bac78631e06d2
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-25 14:51:38 -0600

    add speed test parser

---
+++ script_merge/parse_speedtest.py
@@ -0,0 +1,16 @@
+import datetime
+
+def parse_logfile(logfilename):
+    results = {}
+    with open(logfilename, 'r') as fh:
+        for line in fh:
+            if 'Begin Task: tclean' in line:
+                starttime = datetime.strptime(" ".join(line.split()[:2]),
+                                              "%Y-%m-%d %H-%M-%S")
+            elif 'imagename=' in line:
+                imname = line.split('imagename="')[1].split('"')[0]
+            elif 'End Task: tclean' in line:
+                endtime = datetime.strptime(" ".join(line.split()[:2]),
+                                            "%Y-%m-%d %H-%M-%S")
+                results[imname] = (endtime-starttime).seconds
+    return results
