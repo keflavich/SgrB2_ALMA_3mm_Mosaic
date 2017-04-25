71a2efaacd08640cb6d5431a580477ecb013f4d4
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-24 18:20:21 -0600

    add many more regions, especially Anika's

--- analysis/stellar_mass_estimates.py
+++ analysis/stellar_mass_estimates.py
@@ -43,14 +43,21 @@ clusters = regions.read_ds9(paths.rpath('schmiedeke_clusters.reg'))
 # add in DePree HII regions based on whether or not their names
 # are already in the table, since we didn't count the larger HII regions
 hii_regions = regions.read_ds9(paths.rpath('SgrB2_1.3cm_hiiRegions_masked_Done.reg'))
-for reg in hii_regions:
-    if 'text' not in reg.meta:
-        continue
-    nm = reg.meta['text'].strip("{}")
+hii_regions = Table.read(paths.tpath("Schmiedeke2016_HIIregions_tableB1.txt"), format='ascii.fixed_width')
+
+for row in hii_regions:
+    #if 'text' not in reg.meta:
+    #    continue
+    #nm = reg.meta['text'].strip("{}")
+    nm = row['ID']
+    coord = coordinates.SkyCoord(row['RA'], row['Dec'], frame='fk5', unit=(u.hour, u.deg))
     if nm not in core_phot_tbl['name']:
         core_phot_tbl.add_row({'name': nm, 'SIMBAD_OTYPE':'HII',
-                               'RA': reg.center.ra[0],
-                               'Dec': reg.center.dec[0]})
+                               'RA': coord.ra,
+                               'Dec': coord.dec,
+                               #'RA': reg.center.ra[0],
+                               #'Dec': reg.center.dec[0]
+                              })
 hii = core_phot_tbl['SIMBAD_OTYPE'] == 'HII'
 core_coords = coordinates.SkyCoord(core_phot_tbl['RA'], core_phot_tbl['Dec'],
                                    frame='fk5')
@@ -94,4 +101,13 @@ Cluster M   : N(cores)= 10 N(HII)= 37 counted mass=   1803.50 inferred mass=   6
 Cluster N   : N(cores)= 10 N(HII)=  4 counted mass=    301.72 inferred mass=   1330.33 HII-only inferred mass:    1306.41 core-inferred mass=   1354.25
 Cluster NE  : N(cores)=  4 N(HII)=  0 counted mass=     47.87 inferred mass=    270.85 HII-only inferred mass:       0.00 core-inferred mass=    541.70
 Cluster S   : N(cores)=  3 N(HII)=  1 counted mass=     81.41 inferred mass=    366.44 HII-only inferred mass:     326.60 core-inferred mass=    406.27
+
+Updated 4/24/2017:
+Cluster M   : N(cores)= 10 N(HII)= 55 counted mass=   2622.65 inferred mass=   9658.70 HII-only inferred mass:   17963.15 core-inferred mass=   1354.25
+Cluster N   : N(cores)= 10 N(HII)=  7 counted mass=    438.24 inferred mass=   1820.23 HII-only inferred mass:    2286.22 core-inferred mass=   1354.25
+Cluster NE  : N(cores)=  4 N(HII)=  0 counted mass=     47.87 inferred mass=    270.85 HII-only inferred mass:       0.00 core-inferred mass=    541.70
+Cluster S   : N(cores)=  3 N(HII)=  1 counted mass=     81.41 inferred mass=    366.44 HII-only inferred mass:     326.60 core-inferred mass=    406.27
+
+
+still missing 2 in NE, 5, in M, 1 in S, and have one too many in N
 """
