fe8373a0ff3e6e0cf57efa6b76930679fe3bdede
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-04-25 14:53:45 -0600

    - to :

--- script_merge/parse_speedtest.py
+++ script_merge/parse_speedtest.py
@@ -6,11 +6,11 @@ def parse_logfile(logfilename):
         for line in fh:
             if 'Begin Task: tclean' in line:
                 starttime = datetime.strptime(" ".join(line.split()[:2]),
-                                              "%Y-%m-%d %H-%M-%S")
+                                              "%Y-%m-%d %H:%M:%S")
             elif 'imagename=' in line:
                 imname = line.split('imagename="')[1].split('"')[0]
             elif 'End Task: tclean' in line:
                 endtime = datetime.strptime(" ".join(line.split()[:2]),
-                                            "%Y-%m-%d %H-%M-%S")
+                                            "%Y-%m-%d %H:%M:%S")
                 results[imname] = (endtime-starttime).seconds
     return results
