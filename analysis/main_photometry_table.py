1f7c8dd997ec2f4207ea3369b3cce444c821b0ad
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-17 20:59:56 -0600

    add a new table

--- analysis/main_photometry_table.py
+++ analysis/main_photometry_table.py
@@ -5,6 +5,10 @@ from astropy import coordinates
 from astropy import units as u
 from latex_info import (latexdict, format_float, round_to_n,
                         strip_trailing_zeros, exp_to_tex)
+from astropy.io import fits
+from astropy import wcs
+
+sgrb2contfile = fits.open(paths.Fpath('merge/continuum/SgrB2_selfcal_full_TCTE7m_selfcal5_ampphase_taylorterms_multiscale_deeper_mask2.5mJy.image.tt0.pbcor.fits'))
 
 cont_tbl = Table.read(paths.tpath("continuum_photometry_withSIMBAD.ipac"), format='ascii.ipac')
 
@@ -22,9 +26,10 @@ cont_tbl.add_column(Column(name='$S_{nu,max}$', data=cont_tbl['peak'].to(u.mJy/u
 cont_tbl.remove_column('peak')
 cont_tbl.add_column(Column(name='$T_{B,max}$', data=cont_tbl['peak_K'].to(u.K)))
 cont_tbl.remove_column('peak_K')
+mywcs = wcs.WCS(sgrb2contfile[0].header)
+pix_area = wcs.utils.proj_plane_pixel_area(mywcs)*u.deg**2
 cont_tbl.add_column(Column(name='$S_{nu,tot}$',
-                           data=(cont_tbl['sum']*cont_tbl['beam_area'].to(u.sr)*u.beam).to(u.mJy,
-                                                                                           u.dimensionless_angles())))
+                           data=(cont_tbl['sum']*cont_tbl['beam_area'].to(u.sr)/pix_area*u.beam).to(u.mJy,)))
 cont_tbl.remove_column('sum')
 cont_tbl.add_column(Column(name='$\sigma_{bg}$',
                            data=u.Quantity(cont_tbl['bgmad'],
@@ -43,16 +48,14 @@ cont_tbl.add_column(Column(name='Classification',
                            data=['S' if row['color'] == 'green' else 'W' for row in cont_tbl]))
 cont_tbl.remove_column('color')
 
-cont_tbl.remove_column('npix')
-cont_tbl.remove_column('beam_area')
-cont_tbl.remove_column('peak_90GHz')
-cont_tbl.remove_column('sum_90GHz')
-cont_tbl.remove_column('bgmad_90GHz')
-cont_tbl.remove_column('peak_100GHz')
-cont_tbl.remove_column('sum_100GHz')
-cont_tbl.remove_column('bgmad_100GHz')
+for colname in ['SIMBAD_ID', 'SIMBAD_OTYPE', 'Caswell_Name', 'npix',
+                'beam_area', 'peak_90GHz', 'sum_90GHz', 'bgmad_90GHz',
+                'peak_100GHz', 'sum_100GHz', 'bgmad_100GHz', 'Caswell_V_CH3OH',
+                'Caswell_matchdistance', 'Muno_xray_ID', 'Muno_xray_Counts',
+                'Muno_xray_matchdistance',]:
+    cont_tbl.remove_column(colname)
 
-formats = {'Coordinates': lambda x: x.to_string('hmsdms'),
+formats = {'Coordinates': lambda x: x.to_string('hmsdms', sep=":"),
            '$S_{nu,max}$': lambda x: strip_trailing_zeros('{0:0.2f}'.format(round_to_n(x,2))),
            '$S_{nu,tot}$': lambda x: strip_trailing_zeros('{0:0.2f}'.format(round_to_n(x,2))),
            '$\sigma_{bg}$': lambda x: strip_trailing_zeros('{0:0.2f}'.format(round_to_n(x,2))),
@@ -60,6 +63,7 @@ formats = {'Coordinates': lambda x: x.to_string('hmsdms'),
            '$E[\\alpha]$': lambda x: '-' if np.isnan(x) else strip_trailing_zeros('{0:0.2f}'.format(round_to_n(x,2))),
            '$N(\hh,20 K)$': format_float,
            '$M(20 K)$': lambda x: '-' if np.isnan(x) else strip_trailing_zeros('{0:0.2f}'.format(round_to_n(x,2))),
+           "$T_{B,max}$": lambda x: '-' if np.isnan(x) else strip_trailing_zeros('{0:0.2f}'.format(round_to_n(x,2))),
           }
 
 cont_tbl.write(paths.texpath("continuum_photometry.tex"), formats=formats, overwrite=True,
