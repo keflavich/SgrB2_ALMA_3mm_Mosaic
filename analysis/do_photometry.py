3f4ecdd2eeacbe551606013099208eac0e543b30
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-22 16:27:24 -0600

    fix do_photometry script

--- analysis/do_photometry.py
+++ analysis/do_photometry.py
@@ -1,5 +1,6 @@
 import runpy
-runpy.run_path('core_photometry.py')
-runpy.run_path('merge_core_masers.py')
-runpy.run_path('main_photometry_table.py')
-runpy.run_path('stellar_mass_estimates.py')
+result = runpy.run_path('core_photometry.py', run_name='__main__')
+assert 'tbl' in result
+runpy.run_path('merge_core_masers.py', run_name='__main__')
+runpy.run_path('main_photometry_table.py', run_name='__main__')
+runpy.run_path('stellar_mass_estimates.py', run_name='__main__')
