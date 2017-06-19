1010c5472c8a7bcb34a1dc1aef94d0b7b38aa4cf
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-06-19 12:44:48 -0600

    fix issue with ncores counting incorrectly (previous detections were being double-counted)

--- tex_cores/ncores.py
+++ tex_cores/ncores.py
@@ -3,13 +3,15 @@ from astropy.table import Table
 
 tbl = Table.read('../tables/continuum_photometry_withSIMBAD_andclusters.ipac', format='ascii.ipac')
 
-nhii = sum(['HII' in x for x in tbl['Classification']])
+OK = tbl['peak'] != 0
+nhii = sum([('HII' in x) and k for x,k in zip(tbl['Classification'], OK)])
 xray = np.array(['X' in row['Classification'] for row in tbl], dtype='bool')
 maser = np.array(['M' in row['Classification'] for row in tbl], dtype='bool')
+ncores = OK.sum()
 
 
 with open('ncores.tex', 'w') as fh:
-    fh.write("\\newcommand{{\\ncores}}{{{0}\\xspace}}\n".format(len(tbl)))
+    fh.write("\\newcommand{{\\ncores}}{{{0}\\xspace}}\n".format(ncores))
     fh.write("\\newcommand{{\\nhii}}{{{0}\\xspace}}\n".format(nhii))
     fh.write("\\newcommand{{\\nmasermatch}}{{{0}\\xspace}}\n".format(maser.sum()))
     fh.write("\\newcommand{{\\nxraymatch}}{{{0}\\xspace}}\n".format(xray.sum()))
