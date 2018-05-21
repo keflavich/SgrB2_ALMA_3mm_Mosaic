288d709975b057b666d9000e0f43cf90d97e0e94
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-05-21 15:39:37 +0200

    remove tex_cfe in favor of new submodule SgrB2_CFE

--- tex_cfe/make_apjform.py
+++
@@ -1,7 +0,0 @@
-with open('sgrb2_cores.tex','r') as fhr:
-    with open('sgrb2_cores_apjform.tex', 'w') as fhw:
-        for line in fhr:
-            if 'figures/' in line:
-                fhw.write(line.replace("figures/",""))
-            else:
-                fhw.write(line)
